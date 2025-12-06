2816f7731f691ff1bc6a07430a0a36c4cd90c8a0
Nam Hoàng <namhoang1811@hotmail.com>
2025-12-06 13:35:37 +0700

    Move App.cpp to App.h

--- BookStore/App.h
+++ BookStore/App.h
@@ -5,6 +5,28 @@ struct App {
     List<Book> books;
     // List<Staff> listStaff;
 
-    void init();
-    void run();  // Hàm chạy chính
+    void menu() {
+        while (true) {
+            Print::title("MENU CHUONG TRINH CHINH");
+            cout << "1. Quan ly Sach" << endl;
+            cout << "2. Quan ly Nhan Vien" << endl;
+            cout << "3. Quan ly Khach Hang" << endl;
+            cout << "4. Quan ly Hoa don" << endl;
+            cout << "5. Thong tin" << endl;
+            cout << "0. Thoat" << endl;
+            cout << endl << PRESS_OPTION;
+            char k = _getch();
+            switch ((int)k - 48) {
+            case 1: BookMenu::show(books); break;
+            case 0:
+                Print::title("MENU CHUONG TRINH CHINH");
+                cout << "Cam on da su dung chuong trinh!" << endl;
+                return;
+            }
+        }
+    }
+    void run() {  // Hàm chạy chính
+        books.loadFile();
+        menu();
+    }
 };
